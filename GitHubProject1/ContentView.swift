50710c66263ebd8271a29500e5ec06481dedf0f2
Vachi Kalra <vachikalra@gmail.com>
2025-07-23 11:09:45 -0700

    Changed the text

--- GitHubProject1/ContentView.swift
+++ GitHubProject1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("My favorite color is purple!")
         }
         .padding()
     }

68a2baef545495413a40d071a1285bc79523f78a
Vachi Kalra <vachikalra@gmail.com>
2025-07-23 11:03:01 -0700

    Initial Commit

---
+++ GitHubProject1/GitHubProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProject1App.swift
+//  GitHubProject1
+//
+//  Created by Vachi Kalra on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubProject1Tests/GitHubProject1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProject1Tests.swift
+//  GitHubProject1Tests
+//
+//  Created by Vachi Kalra on 7/23/25.
+//
+
+import Testing
+@testable import GitHubProject1
+
+struct GitHubProject1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

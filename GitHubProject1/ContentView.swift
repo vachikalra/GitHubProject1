4a0e6005992027afb70135151bc8400a4f3c4cbc
Vachi Kalra <vachikalra@gmail.com>
2025-07-23 11:10:50 -0700

    added more text

--- GitHubProject1/ContentView.swift
+++ GitHubProject1/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("My favorite color is purple!")
+            Text("My favorite color is also pink!")
         }
         .padding()
     }
